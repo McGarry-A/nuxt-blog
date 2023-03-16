5e5634fe420e8755e66de2902c506497e46c7f34
Ahmed <atomcgarry@gmail.com>
2023-03-16 08:56:52 +0000

    nav-bar

---
+++ components/ContentWrapper.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="ContentWrapper">
+        <slot />
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.ContentWrapper {
+    display: block;
+    max-width: 1020px;
+    margin: 0 auto;
+}
+</style>
---
+++ components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="Navbar">
+    <content-wrapper>
+      <ul class="Navbar-List">
+        <li class="Navbar-ListItem">
+          <NuxtLink class="Navbar-ListItemLink" to="/">Home</NuxtLink>
+        </li>
+        <li class="Navbar-ListItem">
+          <NuxtLink class="Navbar-ListItemLink" to="/about">Our Story</NuxtLink>
+        </li>
+        <li class="Navbar-ListItem">
+          <NuxtLink class="Navbar-ListItemLink" to="/create-a-post"
+            >Create a Post</NuxtLink
+          >
+        </li>
+        <li class="Navbar-ListItem">
+          <NuxtLink
+            class="Navbar-ListItemLink Navbar-ListItemLinkButton"
+            to="/register"
+            >Sign In</NuxtLink
+          >
+        </li>
+      </ul>
+    </content-wrapper>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.Navbar {
+  padding: 1.4rem 0;
+  &-List {
+    display: flex;
+    list-style-type: none;
+    gap: 2rem;
+    justify-content: flex-end;
+    align-items: center;
+    &Item {
+      &Link {
+        all: initial;
+        cursor: pointer;
+        font-size: 14px;
+
+        &Button {
+          background: rgb(20, 20, 20);
+          padding: 0.45rem 1.2rem;
+          border-radius: 20px;
+          color: white;
+        }
+      }
+    }
+  }
+}
+</style>

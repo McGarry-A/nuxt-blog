a435efd6c872c3e5a1d8084204271c56e0650ceb
Ahmed <atomcgarry@gmail.com>
2023-03-19 20:11:03 +0000

    styled trending posts

--- components/TrendingPost.vue
+++ components/TrendingPost.vue
@@ -1,11 +1,11 @@
 <template>
   <figure class="TrendingPost">
     <div class="TrendingPost-Left">
-      <p class="TrendingPost-LeftNumber">{{ props.index }}</p>
+      <p class="TrendingPost-LeftNumber">0{{ props.index }}</p>
     </div>
     <div class="TrendingPost-Right">
-      <h4 class="TrendingPost-RighTitle">{{ props.title }}</h4>
       <p class="TrendingPost-RightAuthor">{{ props.author }}</p>
+      <h4 class="TrendingPost-RighTitle">{{ props.title }}</h4>
       <p class="TrendingPost-RightFooter">
         {{ props.date }} · {{ props.timeToRead }} min read
       </p>
@@ -27,4 +27,32 @@ const props = defineProps<IArticleDetails>()
 </script>
 
 <style lang="scss" scoped>
+.TrendingPost {
+  display: flex;
+  gap: 1rem;
+
+  &-Left {
+    &Number {
+      color: rgba(230, 230, 230, 1);
+      line-height: 36px;
+      font-size: 30px;
+      font-weight: 500;
+    }
+  }
+
+  &-Right {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    &Author {
+        font-size: 12px;
+    }
+
+    &Footer {
+        font-size: 12px;
+        font-style: italic;
+    }
+  }
+}
 </style>
